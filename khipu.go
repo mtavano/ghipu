786327941ec633d34ff9c6844d01815bd75e11da
mtavano <chelotavano.udp@gmail.com>
2017-01-04 18:23:11 -0300

    Add Banks request to Khipu

--- khipu.go
+++ khipu.go
@@ -1,5 +1,10 @@
 package gokhipu
 
+import (
+	"fmt"
+	"net/http"
+)
+
 var (
 	basePath = "https://khipu.com/api/2.0"
 )
@@ -17,4 +22,25 @@ func NewKhipuClient(secret, receiverID string) *Khipu {
 		Secret:     secret,
 		ReceiverID: receiverID,
 	}
+}
+
+// Banks ...
+func (kc *Khipu) Banks() (*http.Response, error) {
+	requestPath := basePath + "/banks"
+	req, err := http.NewRequest("GET", requestPath, nil)
+	if err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("failed to create request to %s\n%s", requestPath, err))
+	}
+
+	req.Header.Set("Authorization", setAuth(nil, "GET", requestPath, kc.Secret, kc.ReceiverID))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "application/json")
+
+	cl := http.Client{}
+	res, err := cl.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("failed to made request to %s\n%s", requestPath, err))
+	}
+
+	return res, nil
 }
