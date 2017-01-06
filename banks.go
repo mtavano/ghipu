979992d725828fb91f9036d854a9a461cab94564
mtavano <chelotavano.udp@gmail.com>
2017-01-06 18:29:20 -0300

    Complete json tags

--- banks.go
+++ banks.go
@@ -2,10 +2,10 @@ package gokhipu
 
 // Bank represents the schema of bank defined by khipu
 type Bank struct {
-	BankID    string `json:"bank_id"`
-	Name      string `json:"name"`
-	Message   string `json:"message"`
-	MinAmount string `json:"min_amount"`
-	Type      string `json:"type"`
-	Parent    string `json:"parent"`
+	BankID    string `json:"bank_id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Message   string `json:"message,omitempty"`
+	MinAmount string `json:"min_amount,omitempty"`
+	Type      string `json:"type,omitempty"`
+	Parent    string `json:"parent,omitempty"`
 }
