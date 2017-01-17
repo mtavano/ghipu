1dc4ef7452e1831bcc3b84912a67ed2ccf268a45
Jorge Aguirre <jorge.aguirre.sza@gmail.com>
2017-01-17 13:19:03 -0300

    Add basic error definitions

--- khipu.go
+++ khipu.go
@@ -7,11 +7,16 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"strings"
 )
 
-const basePath = "https://khipu.com/api/2.0"
+var baseURL = url.URL{
+	Scheme: "https",
+	Host:   "khipu.com",
+	Path:   "api/2.0",
+}
 
 // Client represents an khipu's REST API.
 type Client struct {
@@ -21,17 +26,31 @@ type Client struct {
 
 // NewClient returns an instance of khipu that is the client to make payment request
 func NewClient(secret, receiverID string) *Client {
-	client := httpClient{
+	hclient := httpClient{
 		client: &http.Client{},
 		secret: secret,
 		recid:  receiverID,
 	}
 
 	return &Client{
-		PaymentService: PaymentService{&client},
-		BankService:    BankService{&client},
+		PaymentService: PaymentService{&hclient},
+		BankService:    BankService{&hclient},
 	}
 }
+
+// ErrorResponse represents an error of the Kiphu's REST API.
+type ErrorResponse string
+
+func (err ErrorResponse) Error() string {
+	return "ghipu: " + string(err)
+}
+
+// Khipu's REST API errors
+const (
+	ErrInvalidPayload       ErrorResponse = "invalid payload"
+	ErrInvalidAuthorization ErrorResponse = "invalid request"
+	ErrInvalidOperation     ErrorResponse = "invalid operation"
+)
 
 type httpClient struct {
 	client *http.Client
@@ -40,19 +59,37 @@ type httpClient struct {
 }
 
 func (hc *httpClient) Do(req *http.Request, values url.Values) (*http.Response, error) {
-	req.Header.Set("Authorization", hc.authParams(req, values))
+	hc.signRequest(req, values)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "application/json")
+
+	reqdump, _ := httputil.DumpRequest(req, true)
+	fmt.Printf("%s\n", reqdump)
 
 	resp, err := hc.client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("client: %s resquest failed, %v", req.URL, err)
 	}
 
-	return resp, nil
+	respdump, _ := httputil.DumpResponse(resp, false)
+	fmt.Printf("%s\n", respdump)
+
+	switch resp.StatusCode {
+	case http.StatusBadRequest:
+		return nil, ErrInvalidPayload
+	case http.StatusForbidden:
+		return nil, ErrInvalidAuthorization
+	case http.StatusServiceUnavailable:
+		return nil, ErrInvalidOperation
+	default:
+		return resp, nil
+	}
 }
 
 func (hc *httpClient) Get(path string, values url.Values) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodGet, path, nil)
+	uri := baseURL.String() + path
+	req, err := http.NewRequest(http.MethodGet, uri, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -60,15 +97,15 @@ func (hc *httpClient) Get(path string, values url.Values) (*http.Response, error
 }
 
 func (hc *httpClient) PostForm(path string, values url.Values) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodPost, path, strings.NewReader(values.Encode()))
+	uri := baseURL.String() + path
+	req, err := http.NewRequest(http.MethodPost, uri, strings.NewReader(values.Encode()))
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	return hc.Do(req, values)
 }
 
-func (hc *httpClient) authParams(req *http.Request, values url.Values) string {
+func (hc *httpClient) signRequest(req *http.Request, values url.Values) {
 	var buff bytes.Buffer
 	buff.WriteString(url.QueryEscape(req.Method))
 	buff.WriteByte('&')
@@ -79,7 +116,9 @@ func (hc *httpClient) authParams(req *http.Request, values url.Values) string {
 		buff.WriteString(strings.Replace(values.Encode(), "+", "%20", -1)[1:])
 	}
 
-	sign := hmac.New(sha256.New, []byte(hc.secret))
-	sign.Write(buff.Bytes())
-	return hc.recid + ":" + hex.EncodeToString(sign.Sum(nil))
+	sig := hmac.New(sha256.New, []byte(hc.secret))
+	sig.Write(buff.Bytes())
+
+	sign := hex.EncodeToString(sig.Sum(nil))
+	req.Header.Set("Authorization", hc.recid+":"+sign)
 }
